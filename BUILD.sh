12b0fb032cb05d46df39a2126b13e2090fb395cc
Eli Heuer <elih@protonmail.com>
2021-05-19 15:32:19 -0700

    Update production build script

--- BUILD.sh
+++ BUILD.sh
@@ -1,5 +1,16 @@
 # !/bin/bash
 set -e
 
-python3 documentation/drawbot/specimen-image-001.py
+echo "**** BUILDING WITH FONTMAKE"
+fontmake -u sources/Mekorot-Rashi-Regular.ufo -o ttf
 
+echo "**** MOVING FONT FILES"
+mkdir -p fonts/ttf
+cp master_ttf/Mekorot-Rashi-Regular.ttf fonts/ttf/Mekorot-Rashi-Regular.ttf
+
+echo "**** BUILDING SPECIMENS"
+python3 documentation/drawbot/specimen-image-001.py
+open documentation/drawbot/specimen-001.png
+
+echo "**** REMOVING BUILD DIRECTORIES"
+rm -rf master_ttf
