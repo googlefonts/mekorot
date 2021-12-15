2a941ea383a3acf8d54f00033bfc1af0e13b51bf
Eli Heuer <elih@protonmail.com>
2021-12-14 20:27:45 -0800

    Rename latin core specimen source file

--- documentation/version-2-specimens/gf-latin-core-glyphset-wide-001.py
+++ documentation/version-2-specimens/gf-latin-core-glyphset-wide-001.py
@@ -1,23 +1,32 @@
 # Render with DrawBot: drawbot.com
 # Use drawbot-skia on Linux systems: https://github.com/justvanrossum/drawbot-skia
+# Easy install(use a virtual environment if possible):
 # $ pip install git+https://github.com/typemytype/drawbot
 from drawBot import *
 
 
 # Constants, these are the main "settings" for the image
-WIDTH = 2048
-HEIGHT = 1024
-MARGIN = 128
+# Adjust multiple to change output-image resolution
+WIDTH = 2048*2
+HEIGHT = 1024*2
+MARGIN = 128*2
+GRID_UNIT = 32*2
 FRAMES = 1
 GRID_VIEW = False  # Change this from "False" to "True" for a grid overlay
 
 
+# Load a font and print font info
+font("v2-fonts/Mekorot[wght].ttf")
+for axis, data in listFontVariations().items():
+    print((axis, data))
+
+
 # Draw a grid
-def grid():
-    stroke(1, 0, 0, 0.3)
+def draw_grid():
+    stroke(1, 0, 0, 0.9)
     strokeWidth(1)
     STEP_X, STEP_Y = 0, 0
-    INCREMENT_X, INCREMENT_Y = MARGIN / 4, MARGIN / 4
+    INCREMENT_X, INCREMENT_Y = GRID_UNIT, GRID_UNIT
     rect(MARGIN, MARGIN, WIDTH - (MARGIN * 2), HEIGHT - (MARGIN * 2))
     for x in range(57):
         polygon((MARGIN + STEP_X, MARGIN), (MARGIN + STEP_X, HEIGHT - MARGIN))
@@ -35,15 +44,9 @@ def draw_background():
     fill(0)
     rect(-2, -2, WIDTH + 2, HEIGHT + 2)
     if GRID_VIEW:
-        grid()
+        draw_grid()
     else:
         pass
-
-
-# Print font info
-font("v2-fonts/Mekorot[wght].ttf")
-for axis, data in listFontVariations().items():
-    print((axis, data))
 
 
 # Draws the image
@@ -52,54 +55,52 @@ def draw_image():
     fill(0.95)
     stroke(None)
     font("v2-fonts/Mekorot[wght].ttf")
-
     fontVariations(wght=400.0)  # Range: 400.0 -> 800.0
-    fontSize(MARGIN * 0.75)
+    fontSize(GRID_UNIT * 3)
     textBox(
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-        (MARGIN, MARGIN * 5.5, 1792, 128 + 24),
+        (MARGIN, GRID_UNIT * 22, WIDTH - MARGIN *2, GRID_UNIT * 4.7),
         align="center",
     )
     textBox(
-        "abcdefghijklmnopqrstuvwxyz æœç",
-        (MARGIN, MARGIN * 4.75, 1792, 128 + 24),
+        "abcdefghijklmnopqrstuvwxyz@æœç",
+        (MARGIN, GRID_UNIT * 19, WIDTH - MARGIN *2, GRID_UNIT * 4.7),
         align="center",
     )
     textBox(
-        "0123456789 !¡?¿&@¼½¾ÆÇÐÞß",
-        (MARGIN, MARGIN * 4, 1792, 128 + 24),
+        "0123456789$€£¥¢¤Æ&§",
+        (MARGIN, GRID_UNIT * 16, WIDTH - MARGIN *2, GRID_UNIT * 4.7),
         align="center",
     )
     textBox(
-        "ÀÁÂÃÄÅÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ",
-        (MARGIN, MARGIN * 3.25, 1792, 128 + 24),
+        "ÁÀÂÃÄÅÉÈÊËÍÌÎÏÑÓÒÔÕÖÚÙÛÜÝ",
+        (MARGIN, GRID_UNIT * 13, WIDTH - MARGIN *2, GRID_UNIT * 4.7),
         align="center",
     )
     textBox(
-        "àáâãäåèéêëìíîïðñòóôõöùúûüýÿıþŒ",
-        (MARGIN, MARGIN * 2.5, 1792, 128 + 24),
+        "áàâãäåéèêëíıìîïðñóòôõöúùûüýÿþŒÇÐÞß",
+        (MARGIN, GRID_UNIT * 10, WIDTH - MARGIN *2, GRID_UNIT * 4.7),
         align="center",
     )
     textBox(
-        "(*)[¦]{|}#$+-/<=>\^_`~¢£¤¥§¨ª«»‹›",
-        (MARGIN, MARGIN * 1.75, 1792, 128 + 24),
+        "(*)[¦]{|}#+-÷=±<>^_`~¨ªº«»‹›",
+        (MARGIN, GRID_UNIT * 7, WIDTH - MARGIN *2, GRID_UNIT * 4.7),
         align="center",
     )
     textBox(
-        ".,:;•…\"'‘’‚„“”−–—¬¯°±²³´µ¶·¸¹ºˆ˚˜⁄⁴€∕%Øø÷©®",
-        (MARGIN, MARGIN * 1, 1792, 128 + 24),
+        "!?¡¿.,:;•…\"'‘’‚„“”−–—¬¯´%¼½¾°¹²³⁴µ¶·Øø©®",
+        (MARGIN, GRID_UNIT * 4, WIDTH - MARGIN *2, GRID_UNIT * 4.7),
         align="center",
     )
-
-    fontSize(MARGIN * 0.25)
+    fontSize(GRID_UNIT * 1)
     textBox(
         "Mekorot Latin Regular Version 2.000: Google Fonts Latin Core Character Set",
-        (MARGIN, MARGIN * 6, 1792, 128),
+        (MARGIN, GRID_UNIT * 23, WIDTH - MARGIN *2, GRID_UNIT * 4.9),
         align="center",
     )
     textBox(
         "SIL Open Font License, Version 1.1 • https://github.com/googlefonts/mekorot",
-        (MARGIN, 32, 1792, 128),
+        (MARGIN, GRID_UNIT * 0, WIDTH - MARGIN *2, GRID_UNIT * 4.9),
         align="center",
     )
 
@@ -108,6 +109,6 @@ def draw_image():
 if __name__ == "__main__":
     newDrawing()
     draw_image()
-    saveImage("gf-latin-core-glyphset-001--2048x1024.png")
+    saveImage("gf-latin-core-glyphset-001.png")
     endDrawing()
     print("DrawBot: Done :-)")
